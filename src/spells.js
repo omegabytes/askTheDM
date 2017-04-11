871b8422b781cc093808f83158d335d027a8efb3
Alex Gaesser <agaesser@gmail.com>
2017-04-11 11:39:45 -0700

    no change

--- src/spells.js
+++ src/spells.js
@@ -3,4 +3,4 @@ module.exports = {
     	"acid splash" : "conjuration cantrip. Casting time: one action. Range: 60 feet. Components: Verbal, Somatic. Duration: Instantaneous. You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within five feet of each other. A target must succceed on a Dexterity saving throw or take 1 d 6 acid damage. This spell's damage increases by 1d6 when you reach 5th level, 11th level, and 17th level.",
     	"aid" : "2nd level abjuration. Casting time: one action. Range: 30 feet. Components: Verbal, Somatic, Material (a tiny strip of white cloth). Duration: 8 hours. Your spell bolsters your allies with toughness and resolve. Choose up to three creatures within range. Each target's hit point maximum and current hit points increase by 5 for the duration. At higher levels: When you cast this spell using a spell slot of third level or higher, a target's hit points increase by an additional 5 for each slot level above second."
     }
-};
+}
