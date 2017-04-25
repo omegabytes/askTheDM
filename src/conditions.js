39eee7c955a800cb21a89cea4d75c111f3ed2261
Alex Gaesser <agaesser@gmail.com>
2017-04-24 17:11:48 -0700

    finalized condition descriptions

--- src/conditions.js
+++ src/conditions.js
@@ -15,15 +15,15 @@ module.exports = {
  		"level 6" : "death"
  	},
  	"frightened" 	: "A frightened creature has disadvantage on ability checks and attack rolls while the source of its fear is within line of sight. The creature can’t willingly move closer to the source of its fear.",
- 	"grappled" 		: "A grappled creature’s speed becom es 0, and it can't benefit from any bonus to its speed. The condition ends if the grappler is incapacitated (see the condition). The condition also ends if an effect removes the grappled creature from the reach of the grappler or grappling effect, such as when a creature is hurled away by the thunderwave spell.",
+ 	"grappled" 		: "A grappled creature’s speed becomes 0, and it can't benefit from any bonus to its speed. The condition ends if the grappler is incapacitated (see the condition). The condition also ends if an effect removes the grappled creature from the reach of the grappler or grappling effect, such as when a creature is hurled away by the thunderwave spell. Attack rolls against the creature have disadvantage, and the creature’s attack rolls have advantage.",
  	"incapacitated" : "An incapacitated creature can’t take actions or reactions.",
- 	"invisible" 	: "",
- 	"paralyzed" 	: "",
- 	"petrified" 	: "",
- 	"poisoned" 		: "",
- 	"prone" 		: "",
- 	"restrained" 	: "",
- 	"stunned" 		: "",
- 	"unconscious" 	: ""
+ 	"invisible" 	: "An invisible creature is impossible to see without the aid of magic or a special sense. For the purpose of hiding, the creature is heavily obscured. The creature’s location can be detected by any noise it makes or any tracks it leaves.",
+ 	"paralyzed" 	: "A paralyzed creature is incapacitated and can’t move or speak. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage. Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.",
+  	"petrified" 	: "A petrified creature is transformed, along with any nonmagical object it is wearing or carrying, into a solid inanimate substance (usually stone). Its weight increases by a factor of ten, and it ceases aging. The creature is incapacitated, can’t move or speak, and is unaware of its surroundings. Attack rolls against the creature have advantage. The creature automatically fails Strength and Dexterity saving throws. The creature has resistance to all damage. The creature is immune to poison and disease, although a poison or disease already in its system is suspended, not neutralized.",
+ 	"poisoned" 		: "A poisoned creature has disadvantage on attack rolls and ability checks.",
+ 	"prone" 		: "A prone creature’s only movement option is to crawl, unless it stands up and thereby ends the condition. The creature has disadvantage on attack rolls. An attack roll against the creature has advantage if the attacker is within 5 feet of the creature. Otherwise, the attack roll has disadvantage.",
+ 	"restrained" 	: "A restrained creature’s speed becomes 0, and it can’t benefit from any bonus to its speed. Attack rolls against the creature have advantage, and the creature’s attack rolls have disadvantage. The creature has disadvantage on Dexterity saving throws.",
+ 	"stunned" 		: "A stunned creature is incapacitated, can’t move, and can speak only falteringly. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage.",
+ 	"unconscious" 	: "An unconscious creature is incapacitated, can’t move or speak, and is unaware of its surroundings The creature drops whatever it’s holding and falls prone. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage. Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature."
  }
 }
