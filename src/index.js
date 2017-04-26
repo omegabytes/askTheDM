58ac6136f9be765ed579317cb345017979d6f3be
Alex Gaesser <agaesser@gmail.com>
2017-04-25 23:51:50 -0700

    added language strings, function framework

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 var Alexa = require('alexa-sdk');
 var APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
 var spells = require('./spells');
+var conditions = require('./conditions');
 
 
 exports.handler = function(event, context, callback) {
@@ -29,6 +30,7 @@ var handlers = {
         var attributeSlot = this.event.request.intent.slots.Attribute;
         var spellName;
         var attributeName;
+
         if (spellSlot && spellSlot.value) {
             spellName = spellSlot.value.toLowerCase();
         }
@@ -47,17 +49,17 @@ var handlers = {
         //if the user asks for the attribute of a spell
         if (spell  && spellAttribute) {
             this.attributes['speechOutput'] = spell[spellAttribute];
-            this.attributes['repromptSpeech'] = this.t("SPELL_REPEAT_MESSAGE");
+            this.attributes['repromptSpeech'] = this.t("REPEAT_MESSAGE");
             this.emit(':tellWithCard', spell[spellAttribute], this.attributes['SKILL_NAME'], cardTitle, spell[spellAttribute]);
         }
 
         //if the user asks only about the spell
         else if (spell && !spellAttribute) {
             this.attributes['speechOutput'] = spell.longDescription;
-            this.attributes['repromptSpeech'] = this.t("SPELL_REPEAT_MESSAGE");
+            this.attributes['repromptSpeech'] = this.t("REPEAT_MESSAGE");
             this.emit(':tellWithCard', spell.longDescription, this.attributes['SKILL_NAME'], cardTitle, spell.longDescription);
         } else {
-            var speechOutput = this.t("SPELL_NOT_FOUND_MESSAGE");
+            var speechOutput = this.t("NOT_FOUND_MESSAGE");
             var repromptSpeech = this.t("SPELL_NOT_FOUND_REPROMPT");
             if (spellName) {
                 speechOutput += this.t("SPELL_NOT_FOUND_WITH_SPELL_NAME", spellName);
@@ -71,6 +73,43 @@ var handlers = {
 
             this.emit(':ask', speechOutput, repromptSpeech);
         }
+    },
+    'ConditionsIntent': function () {
+        var conditionSlot = this.event.request.intent.slots.Condition;
+        var conditionName;
+        var exhaustionLevel;
+
+        if (conditionSlot && conditionSlot.value) {
+            conditionName = conditionSlot.value.to toLowerCase();
+        }
+
+        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), conditionName);
+        var conditons = this.t("CONDITIONS");
+        var conditon  = conditions[conditionName];
+
+        //user requests information on exhaustion
+        if (conditon == 'exhaustion') {
+            //if the user just asks about exhaustion, iterate through each level and read it out loud
+            for (levels : 'exhaustion') {
+
+            }
+        } 
+
+        //otherwise, if the user asks for the level of exhaustion, get the description for the level
+        else if (conditon == 'exhaustion' && exhaustionLevel) {
+
+        }
+
+        //user requests information on conditon
+        else if (conditon) {
+         this.attributes['speechOutput'] = conditon;
+         this.attribute['repromptSpeech'] = this.t("REPEAT_MESSAGE");
+         this.emit(':tellWithCard', conditon, this.attribute['SKILL_NAME'], cardTitle, conditon);
+
+        } else {
+
+        }
+
     },
     'AMAZON.HelpIntent': function () {
         this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
@@ -99,27 +138,31 @@ var handlers = {
 var languageStrings = {
     "en": {
         "translation": {
-            "SPELLS": spells.SPELLS_EN_US,
-            "ATTRIBUTES" : spells.ATTRIBUTES_EN_US,
-            "SKILL_NAME": "Ask the DM",
-            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s fireball? ... Now, what can I help you with.",
-            "WELCOME_REPROMPT": "For instructions on what you can say, please say help me.",
-            "DISPLAY_CARD_TITLE": "%s  - Info for %s.",
-            "HELP_MESSAGE": "You can ask questions such as, what\'s Cure Wounds, or, you can say exit...Now, what can I help you with?",
-            "HELP_REPROMPT": "You can say things like, what\'s Death Ward, or you can say exit...Now, what can I help you with?",
-            "STOP_MESSAGE": "Goodbye!",
-            "SPELL_REPEAT_MESSAGE": "Try saying repeat.",
-            "SPELL_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
-            "SPELL_NOT_FOUND_WITH_SPELL_NAME": "the spell info for %s. ",
-            "SPELL_NOT_FOUND_WITHOUT_SPELL_NAME": "that spell. ",
-            "SPELL_NOT_FOUND_REPROMPT": "What else can I help with?"
+            "SPELLS":                                       spells.SPELLS_EN_US,
+            "ATTRIBUTES" :                                  spells.ATTRIBUTES_EN_US,
+            "CONDITIONS" :                                  conditions.CONDITIONS_EN_US,
+            "SKILL_NAME":                                   "Ask the DM",
+            "WELCOME_MESSAGE":                              "Welcome to %s. You can ask a question like, what\'s the range of fireball? ... Now, what can I help you with.",
+            "WELCOME_REPROMPT":                             "For instructions on what you can say, please say help me.",
+            "DISPLAY_CARD_TITLE":                           "%s  - Info for %s.",
+            "HELP_MESSAGE":                                 "You can ask questions such as, what\'s Cure Wounds, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMPT":                                "You can say things like, what\'s Death Ward, or you can say exit...Now, what can I help you with?",
+            "STOP_MESSAGE":                                 "Goodbye!",
+            "REPEAT_MESSAGE":                               "Try saying repeat.",
+            "NOT_FOUND_MESSAGE":                            "I\'m sorry, I currently do not know ",
+            "NOT_FOUND_REPROMPT":                           "What else can I help with?",
+            "SPELL_NOT_FOUND_WITH_SPELL_NAME":              "the spell info for %s. ",
+            "SPELL_NOT_FOUND_WITHOUT_SPELL_NAME":           "that spell. ",
+            "CONDITION_NOT_FOUND_WITH_CONDITION_NAME" :     "the conditon info for %s. ",
+            "CONDITION_NOT_FOUND_WITHOUT_CONDITION_NAME" :  "that conditon. "
         }
     },
     "en-US": {
         "translation": {
-            "SPELLS" : spells.SPELLS_EN_US,
-            "ATTRIBUTES" : spells.ATTRIBUTES_EN_US,
-            "SKILL_NAME" : "Ask the DM"
+            "SPELLS" :      spells.SPELLS_EN_US,
+            "ATTRIBUTES" :  spells.ATTRIBUTES_EN_US,
+            "CONDITIONS" :  conditions.CONDITIONS_EN_US,
+            "SKILL_NAME" :  "Ask the DM"
         }
     }
 };
