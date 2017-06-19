a8fee4aac98882faae1bfe2eb47d0e7f32a060ab
Alex Gaesser <agaesser@gmail.com>
2017-06-19 16:14:54 -0700

    refactored the languageString change, left it in as commented. Needs debugging

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
 var spells = require('./spells');
 var conditions = require('./conditions');
 //shouldnt this be stored as its own file, just like spells.js?
-var languageStrings = require('./languageStrings');
+// var languageStrings = require('./languageStrings');
 
 
 exports.handler = function(event, context, callback) {
@@ -133,6 +133,38 @@ var handlers = {
         this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
         this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    }
+};
+
+var languageStrings = {
+    "en": {
+        "translation": {
+            "SPELLS":                                       spells.SPELLS_EN_US,
+            "ATTRIBUTES" :                                  spells.ATTRIBUTES_EN_US,
+            "CONDITIONS" :                                  conditions.CONDITIONS_EN_US,
+            "SKILL_NAME":                                   "Ask the DM",
+            "WELCOME_MESSAGE":                              "Welcome to %s. You can ask a question like, what\'s the range of fireball? ... Now, what can I help you with?",
+            "WELCOME_REPROMPT":                             "For instructions on what you can say, please say help me.",
+            "DISPLAY_CARD_TITLE":                           "%s  - Info for %s.",
+            "HELP_MESSAGE":                                 "Ask The DM was created to provide quick reference to many of the mechanics of Dungeons and Dragons. The fastest way to interact with this application is by saying Alexa, Ask The DM and follow with you quesiton. For example, Alexa, ask the dm what is the range of fireball. As of version 1.0, you can get information about conditions and spells. For spells, you can get the following information: casting time, duration, range, components, spell type, short description and long description. If you are in interactive mode, say exit to quit.",
+            "HELP_REPROMPT":                                "You can say things like, what\'s Death Ward, or you can say exit...Now, what can I help you with?",
+            "STOP_MESSAGE":                                 "Goodbye!",
+            "REPEAT_MESSAGE":                               "Try saying repeat.",
+            "NOT_FOUND_MESSAGE":                            "I\'m sorry, I currently do not know ",
+            "NOT_FOUND_REPROMPT":                           "What else can I help with?",
+            "SPELL_NOT_FOUND_WITH_SPELL_NAME":              "the spell info for %s. ",
+            "SPELL_NOT_FOUND_WITHOUT_SPELL_NAME":           "that spell. ",
+            "CONDITION_NOT_FOUND_WITH_CONDITION_NAME" :     "the condition info for %s. ",
+            "CONDITION_NOT_FOUND_WITHOUT_CONDITION_NAME" :  "that condition. "
+        }
+    },
+    "en-US": {
+        "translation": {
+            "SPELLS" :      spells.SPELLS_EN_US,
+            "ATTRIBUTES" :  spells.ATTRIBUTES_EN_US,
+            "CONDITIONS" :  conditions.CONDITIONS_EN_US,
+            "SKILL_NAME" :  "Ask the DM"
+        }
     }
 };
 
