00816f4746d876d6d1a739532c2b0ce75bb83def
Josh Nagel <nageljp27@gmail.com>
2017-06-22 15:00:12 -0700

    added exhaustion level descriptions

---
+++ src/exhaustionLevel.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"EXHAUSTION_LEVELS_EN_US"  : {
+		"level one"		: "Creature has disadvantage on all ability checks.",
+	 	"level two"		: "Creature has their speed halved.",
+	 	"level three"	: "Creature has disadvantage on attack rolls and saving throws.",
+	 	"level four"	: "Creature has Hit point maximum halved.",
+	 	"level five"	: "Creature has speed reduced to 0.",
+	 	"level six"		: "Creature dies."
+	}	
+};
