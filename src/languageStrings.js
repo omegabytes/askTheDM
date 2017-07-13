a52821f8e7c4adb3f7b4ac9e4bf7f913aa6d2213
Alex Gaesser <agaesser@gmail.com>
2017-07-13 12:10:11 -0700

    updated to reflect functionality

--- src/languageStrings.js
+++ src/languageStrings.js
@@ -10,7 +10,7 @@ module.exports = {
             "CONDITIONS" :                                  conditions.CONDITIONS_EN_US,
             "EXHAUSTION_LEVEL" :                            exhaustionLevel.EXHAUSTION_LEVELS_EN_US,
             "SKILL_NAME":                                   "Ask the DM",
-            "WELCOME_MESSAGE":                              "Welcome to Ask the DM. You can ask a question like, what\'s the range of fireball? ... Now, what can I help you with? You can also say help.",
+            "WELCOME_MESSAGE":                              "Welcome to Ask the DM. You can ask questions to get information about spells and conditions. You can say things like, what\'s the range of fireball; or: what does blind affect me?... Now, what can I help you with? You can also say help.",
             "WELCOME_REPROMPT":                             "For instructions on what you can say, please say help me.",
             "DISPLAY_CARD_TITLE":                           "%s  - Info for %s.",
             "HELP_MESSAGE":                                 "Ask the DM was created to provide quick reference to many of the mechanics of Dungeons and Dragons. The fastest way to interact with this application is by saying Alexa, open Ask the DM and follow with your quesition. For example say, Alexa ask Ask the DM what is the range of fireball. As of version 1.0, you can get information about conditions and spells. For spells, you can get the following information: casting time, duration, range, components, spell type, short description and long description. For conditions, simply ask about the conditions name. If you are in interactive mode, say exit to quit. ",
