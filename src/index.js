b0443df9bf2ef0567fc82ab57c44ad0b43c90a66
jnoehren <jeffreynoehren9@gmail.com>
2017-08-11 17:36:05 -0700

    Fixing the session to conform to AWS standards

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ var Alexa = require('alexa-sdk');
 // var APP_ID = undefined;
 var APP_ID = "amzn1.ask.skill.30397146-5043-48df-a40f-144d37d39690";
 var spells = require('./spells');
-var conditions = require('./conditions');
-var items = require('./items');
 var languageStrings = require('./languageStrings');
-var oneShot = true;
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -24,13 +21,13 @@ var handlers = {
     'LaunchRequest': function () {
         // If the user either does not reply to the welcome message or says something that is not
         // understood, they will be prompted again with this text.
-        oneShot = false;
+        this.attributes['continue'] = true;
         this.attributes['speechOutput'] = (languageStrings.en.translation.WELCOME_MESSAGE);
         this.attributes['repromptSpeech'] = languageStrings.en.translation.WELCOME_REPROMPT;
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
     },
     'Unhandled': function (){
-        oneShot = false;
+        this.attributes['continue'] = true;
         this.attributes['speechOutput'] = languageStrings.en.translation.UNHANDLED;
         this.attributes['repromptSpeech'] = languageStrings.en.translation.HELP_REPROMPT;
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
@@ -79,197 +76,11 @@ var handlers = {
         //if we are a one shot question the answer will be provided 
         //as a statement. if not the session will remain open and
         //alexa provide our reprompt speech
-        if(oneShot){
-            this.emit(':tell', this.attributes['speechOutput']);
+        if(this.attributes['continue']){ 
+            this.emit(':ask', this.attributes['speechOutput'] + " " + this.attributes['repromptSpeech']);
         }
         else{
-            this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
-        }
-    },
-    'ConditionsIntent': function () {
-        var conditionSlot = this.event.request.intent.slots.Condition;
-        var conditionName;
-        this.attributes['repromptSpeech'] = languageStrings.en.translation.REPROMPT;
-
-        if (conditionSlot && conditionSlot.value) {
-            conditionName = conditionSlot.value.toLowerCase();
-        }
-
-        var conditions = languageStrings.en.translation.CONDITIONS;
-        var condition  = conditions[conditionName];
-
-        //user requests information on condition
-        if (condition) {
-            this.attributes['speechOutput'] = condition;
-        }
-
-        //otherwise, the user asks for an unknown condition, or Alexa doesn't understand
-        else {
-            var speechOutput = languageStrings.en.translation.NOT_FOUND_MESSAGE;
-
-            if (conditionName) {
-                speechOutput += (languageStrings.en.translation.CONDITION_NOT_FOUND_WITH_CONDITION_NAMED, conditionName);
-            } else {
-                speechOutput += languageStrings.en.translation.CONDITION_NOT_FOUND_WITHOUT_CONDITION_NAME;
-            }
-
-            this.attributes['speechOutput'] = speechOutput;
-        }
-
-        //if we are a one shot question the answer will be provided 
-        //as a statement. if not the session will remain open and
-        //alexa provide our reprompt speech
-        if(oneShot){
             this.emit(':tell', this.attributes['speechOutput']);
-        }
-        else{
-            this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
-        }
-    },
-    'ExhaustionLevelIntent': function () {
-        var exhaustionSlot = this.event.request.intent.slots.Level;
-        var exhaustionLevel;
-
-        this.attributes['repromptSpeech'] = languageStrings.en.translation.REPROMPT;
-
-        if (exhaustionSlot && exhaustionSlot.value) {
-            exhaustionLevel = exhaustionSlot.value.toLowerCase();
-        }
-
-        var exhaustionLevelList = languageStrings.en.translation.EXHAUSTION_LEVEL; 
-        var thisExhaustionLevel  = exhaustionLevelList[exhaustionLevel];
-
-        //user requests information on exhaustion levels
-        if (thisExhaustionLevel) {
-            this.attributes['speechOutput'] = thisExhaustionLevel;
-        }
-
-        //otherwise, the user asks for an unknown exhaustion level, or Alexa doesn't understand
-        else {
-            var speechOutput = languageStrings.en.translation.NOT_FOUND_MESSAGE;
-            var repromptSpeech = languageStrings.en.translation.REPROMPT;
-
-            if (exhaustionLevel) {
-                speechOutput += (languageStrings.en.translation.CONDITION_NOT_FOUND_WITH_CONDITION_NAMED, exhaustionLevel);
-            } else {
-                speechOutput += languageStrings.en.translation.CONDITION_NOT_FOUND_WITHOUT_CONDITION_NAME;
-            }
-            this.attributes['speechOutput'] = speechOutput;
-        }
-
-        //if we are a one shot question the answer will be provided 
-        //as a statement. if not the session will remain open and
-        //alexa provide our reprompt speech
-        if(oneShot){
-            this.emit(':tell', this.attributes['speechOutput']);
-        }
-        else{
-            this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
-        }
-    },
-    'SpellCastIntent': function () {
-        var spellSlot = this.event.request.intent.slots.Spell;
-        var spellName;
-
-        this.attributes['repromptSpeech'] = languageStrings.en.translation.REPROMPT;
-
-        if (spellSlot && spellSlot.value) {
-            spellName = spellSlot.value.toLowerCase();
-        }
-
-        var spells = languageStrings.en.translation.SPELLS;
-        var spell  = spells[spellName];
-
-        //user requests information on casting spell
-        if (spell) {
-            this.attributes['speechOutput'] = spellName + " is a " + spell.spellType + ". You can cast it " + spell.components + ". The spell duration is " + spell.duration + ". " + spell.shortDescription;
-        }
-
-        //otherwise, the user asks for an unknown spells, or Alexa doesn't understand
-        else {
-            var speechOutput = languageStrings.en.translation.NOT_FOUND_MESSAGE;
-
-            if (spell) {
-                speechOutput += (languageStrings.en.translation.SPELL_NOT_FOUND_WITH_SPELL_NAME, spell);
-            } else {
-                speechOutput += languageStrings.en.translation.SPELL_NOT_FOUND_WITHOUT_SPELL_NAME;
-            }
-
-            this.attributes['speechOutput'] = speechOutput;
-        }
-
-        //if we are a one shot question the answer will be provided 
-        //as a statement. if not the session will remain open and
-        //alexa provide our reprompt speech
-        if(oneShot){
-            this.emit(':tell', this.attributes['speechOutput']);
-        }
-        else{
-            this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
-        }
-    },
-    'ItemsIntent': function () {
-        var itemSlot = this.event.request.intent.slots.Item;
-        var itemAttributeSlot = this.event.request.intent.slots.ItemAttribute;
-        var itemName;
-        var itemAttributeName;
-
-        if(itemSlot && itemSlot.value){
-            itemName = itemSlot.value.toLowerCase();
-        }
-
-        if(itemAttributeSlot && itemAttributeSlot.value){
-            itemAttributeName = itemAttributeSlot.value.toLowerCase();
-        }
-
-        var itemList = languageStrings.en.translation.ITEMS;
-        var itemAttributeList= languageStrings.en.translation.ITEM_ATTRIBUTES;
-
-        var item = itemList[itemName];
-        var itemAttribute  = itemAttributeList[itemAttributeName];
-
-        if(item && itemAttribute){
-            if(!item[itemAttribute]){
-                this.attributes['speechOutput'] = languageStrings.en.translation.ATTRIBUTE_DOES_NOT_EXSIST;
-                this.attributes['repromptSpeech'] = languageStrings.en.translation.REPROMPT;
-                this.emit(':ask', this.attributes['speechOutput']);
-                this.emit(':tell', this.attributes['repromptSpeech']);
-            }
-            else{
-                this.attributes['speechOutput'] = item[itemAttribute];
-                this.attributes['repromptSpeech'] = languageStrings.en.translation.REPROMPT;
-                this.emit(':ask', this.attributes['speechOutput']);
-                this.emit(':tell', this.attributes['repromptSpeech']);
-            }
-        }
-
-        else if(item && !itemAttribute){
-            if(item.itemType){
-                this.attributes['speechOutput'] = "It is a "+item.itemType;
-            }
-            else{
-                this.attributes['speechOutput'] = "It is a "+item.category;
-            }
-            this.attributes['repromptSpeech'] = languageStrings.en.translation.REPROMPT;
-            this.emit(':ask', this.attributes['speechOutput']);
-            this.emit(':tell', this.attributes['repromptSpeech']);
-        }
-
-        else {
-            var speechOutput = languageStrings.en.translation.NOT_FOUND_MESSAGE;
-            var repromptSpeech = languageStrings.en.translation.NOT_FOUND_REPROMPT;
-
-            if (item) {
-                speechOutput = (languageStrings.en.translation.CONDITION_NOT_FOUND_WITH_CONDITION_NAMED, item);
-            } else {
-                speechOutput = languageStrings.en.translation.CONDITION_NOT_FOUND_WITHOUT_CONDITION_NAME;
-            }
-            speechOutput = repromptSpeech;
-
-            this.attributes['speechOutput'] = speechOutput;
-            this.attributes['repromptSpeech'] = repromptSpeech;
-            this.emit(':ask', this.attributes['speechOutput']);
-            this.emit(':tell', this.attributes['repromptSpeech']);
         }
     },
     'AMAZON.HelpIntent': function () {
--- src/languageStrings.js
+++ src/languageStrings.js
@@ -1,16 +1,12 @@
 var spells = require('./spells');
-var conditions = require('./conditions');
-var exhaustionLevel = require('./exhaustionLevel');
-var items = require('./items');
 
 module.exports = {
     "en": {
         "translation": {
             "SPELLS":                                       spells.SPELLS_EN_US,
             "ATTRIBUTES" :                                  spells.ATTRIBUTES_EN_US,
-            "CONDITIONS" :                                  conditions.CONDITIONS_EN_US,
-            "EXHAUSTION_LEVEL" :                            exhaustionLevel.EXHAUSTION_LEVELS_EN_US,
             "WELCOME_REPROMPT":                             "For instructions on what you can say, please say help me.",
+            "WELCOME_MESSAGE":                              "Welcome to my handbook. You can ask a question like, what\'s the range of fireball? ... Now, what can I help you with? You can also say help.",
             "DISPLAY_CARD_TITLE":                           "%s  - Info for %s.",
             "HELP_MESSAGE":                                 "Ask the DM was created to provide quick reference to many of the mechanics of Dungeons and Dragons. The fastest way to interact with this application is by saying Alexa, open Ask the DM and follow with your question. For example say, Alexa ask Ask the DM what is the range of fireball. As of version 1.0, you can get information about conditions and spells. For spells, you can get the following information: casting time, duration, range, components, spell type, short description and long description. For conditions, simply ask about the conditions name. If you are in interactive mode, say exit to quit. Now, what was your question?",
             "HELP_REPROMPT":                                "You can say things like, what\'s Death Ward, or you can say exit...Now, what can I help you with?",
@@ -27,9 +23,7 @@ module.exports = {
     },
     "en-US": {
         "translation": {
-            "SPELLS" :      spells.SPELLS_EN_US,
-            "ATTRIBUTES" :  spells.ATTRIBUTES_EN_US,
-            "CONDITIONS" :  conditions.CONDITIONS_EN_US
+            "SPELLS" :      spells.SPELLS_EN_US
         }
     }
 };
