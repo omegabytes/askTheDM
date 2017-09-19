0dc40b141b36a5687a18a1617cc24b09c63fd548
Alex Gaesser <agaesser@gmail.com>
2017-09-18 17:12:15 -0700

    alphabetized alexaLib, added testing strings, spellHealIntent is failing

--- src/alexaLib.js
+++ src/alexaLib.js
@@ -6,11 +6,33 @@ exports.notFoundMessage = function(slotName, userInput) {
 	var speechOutput = langEN.NOT_FOUND_MESSAGE;
     
     if(userInput) {
-    	speechOutput += "the " + slotName + " info for " + userInput;
+    	speechOutput += "the " + slotName + " info for " + userInput + ".";
     }else {
     	speechOutput += langEN.NOT_FOUND_WITHOUT_OBJECT_NAME;
     }
     return speechOutput;
+};
+
+// returns the page(s) where a subject can be found
+// in the 5e player's handbook
+exports.pageFind = function(index, indexName) {
+    var pageString = "";
+    if(typeof index.pages === 'string'){
+        pageString = index.pages
+    }
+    else{
+        if(index.pages.length>1){
+            pageString += "pages ";
+            for(var i = 0; i <= index.pages.length-2; i++){
+                pageString += index.pages[i] + ", "
+            }
+            pageString += "and " + index.pages[index.pages.length-1]
+        }
+        else{
+            pageString = "page " + index.pages
+        }
+    }
+    return indexName + " can be found on " + pageString + ". ";
 };
 
 // roll dice function
@@ -32,24 +54,4 @@ exports.validateAndSetSlot = function(slot) {
 	}else {
 		return null;
 	}
-};
-
-exports.pageFind = function(index, indexName) {
-    var pageString = "";
-    if(typeof index.pages === 'string'){
-        pageString = index.pages
-    }
-    else{
-        if(index.pages.length>1){
-            pageString += "pages ";
-            for(var i = 0; i <= index.pages.length-2; i++){
-                pageString += index.pages[i] + ", "
-            }
-            pageString += "and " + index.pages[index.pages.length-1]
-        }
-        else{
-            pageString = "page " + index.pages 
-        }
-    }
-    return indexName + " can be found on " + pageString;
 };
