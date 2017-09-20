f1010d1c3445b746d3326e7188f5bf8da0db8fee
Alex Gaesser <agaesser@gmail.com>
2017-09-20 14:36:39 -0700

    you can now ask how fast things are

--- src/items.js
+++ src/items.js
@@ -8,6 +8,7 @@ module.exports = {
      "category"       : "category",
      "cost"           : "cost",
      "damage"         : "damage",
+     "fast"           : "speed",
      "item type"      : "itemType",
      "price"          : "cost",
      "properties"     : "properties",
