e49ea9382aaaee66a8295172a11b12b469fc114e
Alex Gaesser <agaesser@gmail.com>
2017-09-26 15:40:32 -0700

    zip and upload script

---
+++ upload.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+
+import os
+import zipfile
+
+def remove(file):
+    if os.path.isfile(file):
+        print "Archive.zip exists, deleting"
+        os.remove(file)
+    else:
+        print "Archive.zip does not exist, continuing"
+
+def zip(src):
+    zf = zipfile.ZipFile("Archive.zip", "w", zipfile.ZIP_DEFLATED)
+    abs_src = os.path.abspath(src)
+
+    for dirname, subdirs, files in os.walk(src):
+        for filename in files:
+            absname = os.path.abspath(os.path.join(dirname,filename))
+            arcname = absname[len(abs_src) + 1:]
+            zf.write(absname, arcname)
+    zf.close()
+
+remove("Archive.zip")
+zip("src")
+os.system("aws s3 cp Archive.zip s3://askthedm/Archive.zip")
+print "https://s3.amazonaws.com/askthedm/Archive.zip"
