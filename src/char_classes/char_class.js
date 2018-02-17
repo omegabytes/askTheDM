429964d4b6a4eba2fd91961ad41d5b072f992c15
Alex Gaesser <agaesser@gmail.com>
2018-02-17 15:55:49 -0800

    Add files via upload

--- src/char_classes/char_class.js
+++ src/char_classes/char_class.js
@@ -1,40 +1,80 @@
-function Char_class() {
-    // class_desc
-        // short
-        // long
+let barbarian       = require('./char_classes/barbarian.js');
+let bard            = require('./char_classes/bard');
+let cleric          = require('./char_classes/cleric');
+let druid           = require('./char_classes/druid');
+let fighter         = require('./char_classes/fighter');
+let monk            = require('./char_classes/monk');
+let paladin         = require('./char_classes/paladin');
+let ranger          = require('./char_classes/ranger');
+let rogue           = require('./char_classes/rogue');
+let sorcerer        = require('./char_classes/sorcerer');
+let warlock         = require('./char_classes/warlock');
+let wizard          = require('./char_classes/wizard');
 
-    // hp
-        // hit dice
-        // 1st level
-        // higher levels
-
-    // primary abilities
-        // ability scores
-        // background
-        // recommended starting spells
-
+class Char_Class {
+  constructor(charClass) {
+    this.charClass = charClass;
+    this.shortDescription = charClass.class_description.shortDescription;
+    this.longDescription = charClass.class_description.longDescription;
+  }
+  
+  // hp
+  health(modifer) {
+    let hitDice = charClass.class_hit_point.hit_dice;
+    let higherLevels = charClass.class_hit_point.higher_levels;
+    let firstLevel = charClass.class_hit_point.first_level;
+    firstLevel = firstLevel.base * firstLevel.mod;
+    return firstLevel;
+  }
+  
+  // primary abilities
+  abilities() {
+    // ability scores
+    // background
+    // recommended starting spells
+  }
+  
+  
+  
     // proficiencies
-        // armor
-        // weapon
-        // tools
-        // saving throws
-        // skills
-
+  proficiencies() {
+    // armor
+    // weapon
+    // tools
+    // saving throws
+    // skills
+  }
+  
     // equipment
-        // starting pack
-        // weapon choices
-        // equipment pack choices
-        // other equipment
+  equipment() {
+    // starting pack
+    // weapon choices
+    // equipment pack choices
+    // other equipment
+  }
+
 
     // list of spells
-
-    // spellcasting ability
-        // spell save dc
-        // spell attack modifier
-        // spell focus
-
-    // class features
-        // class skills
-        // subclasses
+  spells() {
+  
+  }
+  
+  // spellcasting ability
+  spellcastingAbility() {
+    // spell save dc
+    // spell attack modifier
+    // spell focus
+  
+  }
+  
+  // class features
+  classFeatures() {
+    // class skills
+    // subclasses
+  }
 }
 
+//export Char_Class;
+
+
+let myBard = new Char_Class("bard");
