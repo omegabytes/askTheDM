17afebb4bb31be9d09ad5626613c8789aeb9bd76
Josh Nagel <nageljp27@gmail.com>
2018-03-02 14:20:41 -0800

    added file to handle magic classes, following syntax provided by ageasser

---
+++ src/char_classes/magic_class.js
@@ -0,0 +1,26 @@
+class Magic_Class extends Char_Class {
+	constructor(charClass) {
+		// spells
+		let spellList;
+		let spellSaveDC = charClass.class_spellcasting_ability.spell_save_dc;
+		let spellAttackMod = charClass.class_spellcasting_ability.spell_attack_mod;
+		let SpellFocus = charClass.class_spellcasting_ability.spell_focus;
+
+		super();
+	}
+
+	// get spell list
+	getSpellList() {
+		// parse the spells json and add any spells that match this.className
+		// "spellClass": ["druid", "sorcerer", "wizard"],
+		this.spellList = [];
+	}
+
+	// list spells available at the slot level
+	// params: slot level
+	// output: list of spells
+	getSpellsAtSlotLevel(slotLevel) {
+
+	}
+
+}
