4baea2b0cb8ede6907a277560e8f013408999d81
Josh Nagel <nageljp27@gmail.com>
2018-03-03 11:53:33 -0800

    added equipment pack file to make player class implementation easier.

---
+++ src/equipmentPacks.js
@@ -0,0 +1,99 @@
+module.exports = {
+	"burglar\'s pack":{
+		"items": [
+			{"name": "backpack", "quantity": 1},
+			{"name": "bag of 1,000 ball bearings", "quantity": 1},
+			{"name": "10ft of string", "quantity": 1},
+			{"name": "bell", "quantity": 1},
+			{"name": "candles", "quantity": 5},
+			{"name": "crowbar", "quantity": 1},
+			{"name": "hammer", "quantity": 1},
+			{"name": "pitons", "quantity": 10},
+			{"name":"hooded lantern","quantity":1},
+			{"name":"flask of oil","quantity":2},
+			{"name":"rations","quantity":5},
+			{"name":"tinderbox","quantity":1},
+			{"name":"waterskin","quantity":1},
+			{"name":"hempen rope: 50ft","quantity":1},
+			{"name": "gold pieces", "quantity": 16}
+		]
+	},
+	"diplomat\'s pack":{
+		"items": [
+			{"name": "backpack", "quantity": 1},
+			{"name": "bedroll", "quantity": 1},
+			{"name": "mess kit", "quantity": 1},
+			{"name": "tinderbox", "quantity": 1},
+			{"name": "torches", "quantity": 10},
+			{"name": "rations", "quantity": 10},
+			{"name": "waterskin", "quantity": 1},
+			{"name": "hempen rope: 50ft", "quantity": 1},
+			{"name": "gold pieces", "quantity": 10}
+		]
+	},
+	"dungeoneer\'s pack":{
+		"items": [
+			{"name": "backpack", "quantity": 1},
+			{"name": "bedroll", "quantity": 1},
+			{"name": "mess kit", "quantity": 1},
+			{"name": "tinderbox", "quantity": 1},
+			{"name": "torches", "quantity": 10},
+			{"name": "rations", "quantity": 10},
+			{"name": "waterskin", "quantity": 1},
+			{"name": "hempen rope: 50ft", "quantity": 1},
+			{"name": "gold pieces", "quantity": 10}
+		]
+	},
+	"entertainer\'s pack":{
+		"items": [
+			{"name": "backpack", "quantity": 1},
+			{"name": "bedroll", "quantity": 1},
+			{"name": "mess kit", "quantity": 1},
+			{"name": "tinderbox", "quantity": 1},
+			{"name": "torches", "quantity": 10},
+			{"name": "rations", "quantity": 10},
+			{"name": "waterskin", "quantity": 1},
+			{"name": "hempen rope: 50ft", "quantity": 1},
+			{"name": "gold pieces", "quantity": 10}
+		]
+	},
+	"explorer\'s pack":{
+		"items": [
+			{"name": "backpack", "quantity": 1},
+			{"name": "bedroll", "quantity": 1},
+			{"name": "mess kit", "quantity": 1},
+			{"name": "tinderbox", "quantity": 1},
+			{"name": "torches", "quantity": 10},
+			{"name": "rations", "quantity": 10},
+			{"name": "waterskin", "quantity": 1},
+			{"name": "hempen rope: 50ft", "quantity": 1},
+			{"name": "gold pieces", "quantity": 10}
+		]
+	},
+	"priest\'s pack":{
+		"items": [
+			{"name": "backpack", "quantity": 1},
+			{"name": "bedroll", "quantity": 1},
+			{"name": "mess kit", "quantity": 1},
+			{"name": "tinderbox", "quantity": 1},
+			{"name": "torches", "quantity": 10},
+			{"name": "rations", "quantity": 10},
+			{"name": "waterskin", "quantity": 1},
+			{"name": "hempen rope: 50ft", "quantity": 1},
+			{"name": "gold pieces", "quantity": 10}
+		]
+	},
+	"scholar\'s pack":{
+		"items": [
+			{"name": "backpack", "quantity": 1},
+			{"name": "bedroll", "quantity": 1},
+			{"name": "mess kit", "quantity": 1},
+			{"name": "tinderbox", "quantity": 1},
+			{"name": "torches", "quantity": 10},
+			{"name": "rations", "quantity": 10},
+			{"name": "waterskin", "quantity": 1},
+			{"name": "hempen rope: 50ft", "quantity": 1},
+			{"name": "gold pieces", "quantity": 10}
+		]
+	}
+};
