9e940a354579f36e20ba390e64bc5c4b58f14c07
Josh Nagel <nageljp27@gmail.com>
2018-03-03 11:26:07 -0800

    completed the prototype for Bard.js

--- src/char_classes/Bard.js
+++ src/char_classes/Bard.js
@@ -64,74 +64,87 @@ function Bard(prof_bon, char_mod, con_mod) {
 	this.spell_focus = this.musical_inst;                     // spell focus
 	// class features
 	this.class_specific_ability_1 = {
-		"name": "", "desc": "", "benefits": "", "restore": "",
+		"name": "bardic inspiration",
+		"desc": "You can inspire others through stirring words or music. To do so, you use a Bonus Action on your turn to choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d6.",
+		"benefits": "Once within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, Attack roll, or saving throw it makes.",
+		"restore": "You can use this feature a number of times equal to your Charisma modifier. (a minimum of once). You regain any expended uses when you finish a Long Rest. Your Bardic Inspiration die changes when you reach certain levels in this class. The die becomes a d8 at 5th level, a d10 at 10th level, and a d12 at 15th level.",
 	};
 	this.class_specific_ability_2 = {
-		"name": "", "desc": ""
+		"name": "jack of all trades",
+		"desc": "Starting at 2nd level, you can add half your proficiency bonus, rounded down, to any ability check you make that doesn't already include your proficiency bonus."
 	};
 	this.class_specific_ability_3 = {
-		"name": "", "desc": ""
-	};
-	this.class_specific_ability_4 = {
-		"name": "", "desc": ""
+		"name": "song of rest",
+		"desc": "Starting at 2nd level, you can revitalize wounded allies during a short rest. Any friendly creature that spent Hit Dice to regain hit points, each creature regains an extra 1d6 hit points. This extra hit points increase when you reach certain levels: 1d8 at 9th level, to 1d10 at 13th level, and to 1d12 at 17th level."
 	};
 	// class skills
 	this.ability_score_improvement = {
 		"levels": [4, 8, 12, 16, 19], "benefit": "Increase one ability score by 2, or increase 2 ability scores by 1."
 	};
-	this.extra_attack = "Beginning at 5th level, you can attack twice, instead of once, whenever you take the Attack action on your turn.";
+	this.extra_attack = "Bards do not get an extra attack by default. Please look at the subclass for bards, College of Valor for this ability.";
 	this.class_skill_1 = {
-		"name": "", "desc": ""
+		"name": "expertise",
+		"desc": "At 3rd level, choose two of your skill proficiencies. Your proficiency bonus is doubled for any ability check you make that uses either of the chosen proficiencies. At 10th level, you can choose another two skill proficiencies to gain this benefit."
 	};
 	this.class_skill_2 = {
-		"name": "", "desc": ""
+		"name": "font of inspiration",
+		"desc": "Beginning when you reach 5th level, you regain all of your expended uses of Bardic Inspiration when you finish a short or Long Rest."
 	};
 	this.class_skill_3 = {
-		"name": "", "desc": ""
+		"name": "countercharm",
+		"desc": "At 6th level, as an action, you can start a performance that lasts until the end of your next turn. During that time, you and any friendly creatures within 30 feet of you have advantage on saving throws against being Frightened or Charmed. A creature must be able to hear you to gain this benefit. The performance ends early if you are Incapacitated or silenced or if you voluntarily end it (no action required)."
 	};
 	this.class_skill_4 = {
-		"name": "", "desc": ""
+		"name": "magical secrets",
+		"desc": "By 10th level, you have plundered magical knowledge from a wide spectrum of disciplines. Choose two Spells from any class, including this one. A spell you choose must be of a level you can cast or a cantrip. You learn two additional Spells from any class at 14th level and again at 18th level."
 	};
 	this.class_skill_5 = {
-		"name": "", "desc": ""
-	};
-	this.class_skill_6 = {
-		"name": "", "desc": ""
-	};
-	this.class_skill_7 = {
-		"name": "", "desc": ""
+		"name": "superior inspiration",
+		"desc": "At 20th level, when you roll initiative and have no uses of Bardic Inspiration left, you regain one use."
 	};
 	// subclasses
 	this.subclass_description = {
-		"name": "", "desc": "",
+		"name": "bard college",
+		"desc": "At 3rd level, you delve into the advanced techniques of a bard college of your choice.",
 	};
 	this.subclass_1 = {
-		"name": "path of the berserker",
-		"desc": "For some barbarians, rage is a means to an end, that end being violence. The path of the berserker is a path of untrammeled fury, slick with blood. As you enter the berserker's rage, you thrill in the chaos of battle, heedless of your own health or well-being.",
+		"name": "college of lore",
+		"desc": "Bards of the College of Lore know something about most things, collecting bits of knowledge from sources as diverse as scholarly tomes and peasant tales. Whether singing folk ballads in taverns or elaborate compositions in royal courts, these bards use their gifts to hold audiences spellbound.",
 		"feature_1": {
-			"name": "", "desc": ""
+			"name": "bonus proficiencies",
+			"desc": "When you join the College of Lore at 3rd level, you gain proficiency with three skills of your choice."
 		},
 		"feature_2": {
-			"name": "", "desc": ""
+			"name": "cutting words",
+			"desc": "At 3rd level, you learn how to use your wit to distract, confuse, and otherwise sap the confidence and competence of others. When a creature that you can see within 60 feet of you makes an Attack roll, an ability check, or a damage roll, you can use your reaction to expend one of your uses of Bardic Inspiration, rolling a Bardic Inspiration die and subtracting the number rolled from the creature’s roll."
 		},
 		"feature_3": {
-			"name": "", "desc": ""
+			"name": "additional magical secrets",
+			"desc": "At 6th level, you learn two Spells of your choice from any class. A spell you choose must be of a level you can cast, as shown on the Bard table, or a cantrip. The chosen Spells count as bard Spells for you but don’t count against the number of bard Spells you know."
 		},
 		"feature_4": {
-			"name": "", "desc": ""
+			"name": "peerless skill",
+			"desc": "Starting at 14th level, when you make an ability check, you can expend one use of Bardic Inspiration. Roll a Bardic Inspiration die and add the number rolled to your ability check. You can choose to do so after you roll the die for the ability check, but before the GM tells you whether you succeed or fail."
 		}
 	};
 	this.subclass_2 = {
-		"name": "", "desc": "", "feature_1": {
-			"name": "", "desc": ""
-		}, "feature_2": {
-			"name": "", "desc": "",
-		}, "feature_3": {
-			"name": "", "desc": "",
-		}, "feature_4": {
-			"name": "", "desc": ""
-		}, "feature_5": {
-			"name": "", "desc": "",
+		"name": "college of valor",
+		"desc": "Bards of the College of Valor are daring skalds whose tales keep alive the memory of the great heroes of the past, and thereby inspire a new generation of heroes. These bards gather in mead halls or around great bonfires to sing the deeds of the mighty, both past and present.",
+		"feature_1": {
+			"name": "bonus proficiencies",
+			"desc": "When you join the College of Valor at 3rd level, you gain proficiency with medium armor, shields, and martial weapons."
+		},
+		"feature_2": {
+			"name": "combat inspiration",
+			"desc": "At 3rd level, you learn to inspire others in battle. A creature that has a Bardic Inspiration die from you can roll that die and add the number rolled to a weapon damage roll it just made. Alternatively, when an attack roll is made against the creature, it can use its reaction to roll the Bardic Inspiration die and add the number rolled to its AC against that attack, after seeing the roll but before knowing whether it hits or misses.",
+		},
+		"feature_3": {
+			"name": "extra attack",
+			"desc": "Starting at 6th level, you can attack twice, instead of once, whenever you take the Attack action on your turn.",
+		},
+		"feature_4": {
+			"name": "battle magic",
+			"desc": "At 14th level, you have mastered the art of weaving spellcasting and weapon use into a single harmonious act. When you use your action to a cast a bard spell, you can make one weapon attack as a bonus action."
 		}
 	};
 }
