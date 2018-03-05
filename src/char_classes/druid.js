39963b8c3d026c977b895104ddcbb0e65e70d279
Josh Nagel <nageljp27@gmail.com>
2018-03-05 11:18:38 -0800

    completed Playerclass object prototype.

--- src/char_classes/druid.js
+++ src/char_classes/druid.js
@@ -78,23 +78,79 @@ module.exports = {
 					"rule_5": "You choose whether your equipment falls to the ground in your space, merges into your new form, or is worn by it. Worn equipment functions as normal, but the DM decides whether it is practical for the new form to wear a piece of equipment, based on the creatures shape and size. Your equipment doesnt change size or shape to match the new form, and any equipment that the new form cant wear must either fall to the ground or merge with it. Equipment that merges with the form has no effect until you leave the form."
 				}
 			},
-			"class_specific_ability_2": "",
-			"class_specific_ability_3": "",
-			"class_specific_ability_4": ""
+			"timeless body": "starting at 18th level, the primal magic that you wield causes you to age more slowly. For every 10 years that pass, your body ages only 1 year.",
+			"beast spells": "beginning at 18th level, you cast many of your druid spells in any shape you assume using wild shape. You can perform the somatic and verbal components of a druid spell while in a beast shape, but you arent able to provide material components.",
+			"archdruid": "at 20th level, you can use your wild shape an unlimited number of times."
 		}, "subclass": {
-			"subclass_paths": {
-				"subclass_description": "", "subclass_type_1": {
-					"subclass_1_description": "",
-					"subclass_benefit_1": "",
-					"subclass_benefit_2": "",
-					"subclass_benefit_3": "",
-					"subclass_benefit_4": ""
-				}, "subclass_type_2": {
-					"subclass_2_description": "",
-					"subclass_benefit_1": "",
-					"subclass_benefit_2": "",
-					"subclass_benefit_3": "",
-					"subclass_benefit_4": ""
+			"druid circle": {
+				"description": "at 2nd level, you choose to identify with a circle of druids: the circle of the land or the circle of the moon. Your choice grants you features at 2nd level and again at 6th, 10th, and 14th level.",
+				"circle of the land": {
+					"description": "The Circle of the Land is made up of mystics and sages who safeguard ancient knowledge and rites through a vast oral tradition. These druids meet within sacred circles of trees or standing stones to whisper primal secrets in Druidic. The circle's wisest members preside as the chief priests of communities that hold to the Old Faith and serve as advisors to the rulers of those folk. As a member of this circle, your magic is influenced by the land where you were initiated into the circle's mysterious rites.",
+					"bonus cantrip": "When you choose this circle at 2nd level, you learn one additional druid cantrip of your choice.",
+					"Natural Recovery": "Starting at 2nd level, you can regain some of your magical energy by sitting in meditation and communing with nature. During a short rest, you choose expended spell slots to recover. The spell slots can have a combined level that is equal to or less than half your druid level (rounded up), and none of the slots can be 6th level or higher. You can't use this feature again until you finish a long rest.",
+					"Circle Spells": {
+						"desc": "Your mystical connection to the land infuses you with the ability to cast certain spells. At 3rd, 5th, 7th, and 9th level you gain access to circle spells connected to the land where you became a druid. Choose that land—arctic, coast, desert, forest, grassland, mountain, swamp, or Underdark. Once you gain access to a circle spell, you always have it prepared, and it doesn't count against the number of spells you can prepare each day. If you gain access to a spell that doesn't appear on the druid spell list, the spell is nonetheless a druid spell for you.",
+						"arctic": {
+							3: ["hold person","spike growth"],
+							5: ["sleet storm","slow"],
+							7: ["freedom of movement","ice storm"],
+							9: ["commune with nature","cone of cold"]
+						},
+						"coast": {
+							3: ["mirror image","misty step"],
+							5: ["water breathing","water walk"],
+							7: ["control water","freedom of movement"],
+							9: ["conjure elemental","scrying"]
+						},
+						"desert": {
+							3: ["blur","silence"],
+							5: ["create food and water","protection from energy"],
+							7: ["blight","hallucinatory terrain"],
+							9: ["insect plague","wall of stone"]
+						},
+						"forest": {
+							3: ["barkskin","spider climb"],
+							5: ["climb lightning","plant growth"],
+							7: ["divination","freedom of movement"],
+							9: ["commune with nature","tree stride"]
+						},
+						"grassland": {
+							3: ["invisibility","pass without trace"],
+							5: ["daylight","haste"],
+							7: ["divination","freedom of movement"],
+							9: ["dream","insect plague"]
+						},
+						"mountain": {
+							3: ["spider climb","spike growth"],
+							5: ["lightning bolt","meld into stone"],
+							7: ["stone shape","stoneskin"],
+							9: ["passwall","wall of stone"]
+						},
+						"swamp": {
+							3: ["darkness","melf\'s acid arrow"],
+							5: ["water walk","stinking cloud"],
+							7: ["freedom of movement","locate creature"],
+							9: ["insect plague","scrying"]
+						},
+						"underdark": {
+							3: ["spider climb","web"],
+							5: ["gaseous form","stinking cloud"],
+							7: ["greater invisibility","stone shape"],
+							9: ["cloudkill","insect plague"]
+						},
+
+					},
+					"land\'s stride": "Starting at 6th level, moving through nonmagical difficult terrain costs you no extra movement. You can also pass through nonmagical plants without being slowed by them and without taking damage from them if they have thorns, spines, or a similar hazard. In addition, you have advantage on saving throws against plants that are magically created or manipulated to impede movement, such those created by the entangle spell.",
+					"nature\'s ward":"When you reach 10th level, you can't be charmed or frightened by elementals or fey, and you are immune to poison and disease.",
+					"nature\'s sanctuary":"When you reach 14th level, creatures of the natural world sense your connection to nature and become hesitant to attack you. When a beast or plant creature attacks you, that creature must make a Wisdom saving throw against your druid spell save DC. On a failed save, the creature must choose a different target, or the attack automatically misses. On a successful save, the creature is immune to this effect for 24 hours. The creature is aware of this effect before it makes its attack against you."
+				},
+				"circle of the moon": {
+					"description": "Druids of the Circle of the Moon are fierce guardians of the wilds. Their order gathers under the full moon to share news and trade warnings. They haunt the deepest parts of the wilderness, where they might go for weeks on end before crossing paths with another humanoid creature, let alone another druid. Changeable as the moon, a druid of this circle might prowl as a great cat one night, soar over the treetops as an eagle the next day, and crash through the undergrowth in bear form to drive off a trespassing monster. The wild is in the druid's blood.",
+					"combat wild shape": "When you choose this circle at 2nd level, you gain the ability to use Wild Shape on your turn as a bonus action, rather than as an action. Additionally, while you are transformed by Wild Shape, you can use a bonus action to expend one spell slot to regain 1d8 hit points per level of the spell slot expended.",
+					"circle forms": "The rites of your circle grant you the ability to transform into more dangerous animal forms. Starting at 2nd level, you can use your Wild Shape to transform into a beast with a challenge rating as high as 1 (you ignore the Max CR column of the Beast Shapes table, but must abide by the other limitations there). Starting at 6th level, you can transform into a beast with a challenge rating as high as your druid level divided by 3, rounded down.",
+					"primal strike": "Starting at 6th level, your attacks in beast form count as magical for the purpose of overcoming resistance and immunity to nonmagical attacks and damage.",
+					"elemental wild shape": "At 10th level, you can expend two uses of Wild Shape at the same time to transform into an air elemental, an earth elemental, a fire elemental, or a water elemental.",
+					"Thousand Forms":"By 14th level, you have learned to use magic to alter your physical form in more subtle ways. You can cast the alter self spell at will."
 				}
 			}
 		}
